cae69b5ff31aa6113e35672e38dcf7ac50b89391
Sho Fujiwara <shofujiwara505@gmail.com>
2023-01-24 17:30:19 +0900

    connected js

--- js.js
+++ js.js
@@ -1,3 +1,7 @@
-let givenWord = document.querySelector(".word-input");
+let givenWord = document.querySelector("#word-input");
+let input = document.querySelector("#submit-butt");
 
+input.addEventListener("click", () => {
+  console.log(givenWord.value);
+});
 console.log(givenWord.value);
