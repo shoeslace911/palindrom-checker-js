2d9e54428a495c9871208c97f4096e93c40b268c
Sho Fujiwara <shofujiwara505@gmail.com>
2023-01-26 20:53:17 +0900

    reverse the letter

--- js.js
+++ js.js
@@ -2,6 +2,11 @@ let givenWord = document.querySelector("#word-input");
 let input = document.querySelector("#submit-butt");
 
 input.addEventListener("click", () => {
-  let givenWordValue = givenWord.value;
+  let givenWordValue = givenWord.value.toLowerCase();
+  let sliced = givenWordValue.split("");
+  let reverse = [];
+  for (let i = 0; i < sliced.length; i++) {
+    reverse.unshift(sliced[i]);
+    console.log(reverse);
+  }
 });
-console.log(givenWord.value);
